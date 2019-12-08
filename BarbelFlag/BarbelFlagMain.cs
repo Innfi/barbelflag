3b07a22209a8984a05850f06fa961c3dc53815eb
innfi <innfi.world@gmail.com>
2019-12-09 00:08:04 +0900

    BarbelFlag: initial commit

---
+++ BarbelFlag/BarbelFlagMain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BarbelFlag
+{
+    public class GlobalSetting
+    {
+        public int WinScore = 1000;
+    }
+
+    public enum GameStatus
+    {
+        Initial = 0,
+        End = 100
+    }
+
+    public class GameInstance
+    {
+        public GameStatus Status { get; private set; }
+        public void DummyStatusChanger()
+        {
+            Status = GameStatus.End;
+        }
+    }
+
+    public class Team
+    {
+        public class Initializer
+        {
+            public GlobalSetting Setting;
+            public GameInstance Game;
+        }
+
+        private GlobalSetting globalSetting;
+        private GameInstance gameInstance;
+
+        public Team(Initializer initializer)
+        {
+            globalSetting = initializer.Setting;
+            gameInstance = initializer.Game;
+        }
+
+        public int Score { get; private set; }
+
+        public void RaiseScoreDummy()
+        {
+            Score = globalSetting.WinScore;
+            gameInstance.DummyStatusChanger();
+        }
+    }
+
+    //public class Field
+    //{
+    //}
+
+
+    public static class BarbelFlagMain
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ BarbelFlagTest/UnitTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BarbelFlag;
+
+/*
+TODO
+--------------------------------------------------
+Definition:
+* game instance
+* - instantiate teams
+* - instantiate flags 
+* - manage team scores / win condition
+* - system io
+* 
+* team
+* - capture flags 
+* - raise score
+* - manage team members 
+* 
+* flags
+* - status: neutral / capturing  / captured
+* player
+* - basic stats
+* - character type
+* - skill
+* environments
+* field
+
+
+DONE
+--------------------------------------------------
+*/
+
+namespace BarbelFlagTest
+{
+    [TestClass]
+    public class BarbelFlagTester
+    {
+        [TestMethod]
+        public void Test0InitTeam()
+        {
+            var globalSetting = new GlobalSetting();
+            Assert.AreEqual(globalSetting.WinScore, 2000);
+
+            var team = new Team(new Team.Initializer
+            {
+                Setting = globalSetting
+            });
+        }
+
+        [TestMethod]
+        public void Test1NotifyGameInstanceFromTeam()
+        {
+            var globalSetting = new GlobalSetting();
+
+            var gameInstance = new GameInstance();
+            var team = new Team(new Team.Initializer
+            {
+                Setting = globalSetting,
+                Game = gameInstance
+            });
+
+            Assert.AreEqual(gameInstance.Status, GameStatus.Initial);
+            team.RaiseScoreDummy();
+            Assert.AreEqual(gameInstance.Status, GameStatus.End);
+        }
+    }
+}
